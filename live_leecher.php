8ad047d470c62e5460763deba3ee7ee100d1dfc5
YouCantGetME <khan.wang@gmail.com>
2020-09-22 15:58:24 +0800

    Update live_leecher.php
    Add custom ffmpeg options
    By passing AD at beginning to prevent incorrect video format

--- live_leecher.php
+++ live_leecher.php
@@ -1,65 +1,95 @@
 <?php
 set_time_limit(0);
-define('OAUTH_TOKEN','undefined'); //replace 'undefined' with your oauth_token if you are subscriber to avoid AD
-define('AUDIO_ONLY',0);// set 1 to record audio only , useful to prevent DMCA mute on VOD.
-define('VOD_FOLDER','VOD');
-define('IDLE_TIME',180); //interval between check ,unit second
+define('OAUTH_TOKEN','undefined'); //replace 'undefined' with your oauth_token if you are subscriber to avoid AD , this token could only obtain via 'auth-token' in the Cookie located in Application tab of browser developer tools.
+define('IDLE_TIME',180); //interval between check ,unit second.
+define('VIDEO_CONTAINER','mp4'); //(mkv)Matroska file could still read after accidentally crash.
+define('FFMPEG_OPTIONS','-movflags faststart'); //faststart works on MP4 only.
 define('LOG_FILE','log.txt');
+define('VOD_FOLDER','VOD');
+define('AUDIO_ONLY',0); //set 1 to record audio only , useful to prevent DMCA muting VODs.
+define('TIMEZONE',8); //GMT +8
+
 if(empty($argv[1]))exit('No CHANNEL assigned');
 $channel=$argv[1];
 $first_run=true;
 $session_ts=0;
-$current_ts=time()+28800;
 $lastest_vod_ts=0;
 $ffmpeg_arg='';
 $filename_append='';
 $record_mode='';
 $audio=true;
 $video=true;
-if(AUDIO_ONLY)$video=false;
+$token_status='';
+$timezone_offset=TIMEZONE*3600;
 
-//parse input args , leaves default setting if no args 
-if(isset($argv[2]) && !empty($argv[2]))
+if(!is_dir(VOD_FOLDER))mkdir(VOD_FOLDER);
+
+//AV args will be ignored if codec assigned in FFMPEG_OPTIONS
+if(strpos(FFMPEG_OPTIONS,'-c')===false && strpos(FFMPEG_OPTIONS,'codec')===false)
 {
-	stristr($argv[2],'a')===false?$audio=false:$audio=true;
-	stristr($argv[2],'v')===false?$video=false:$video=true;
-}
-if($audio && $video) 
-{
-	//default , record both audio and video
-	$ffmpeg_arg='-c copy';
-}
-elseif($audio)
-{
-	//audio only
-	$ffmpeg_arg='-vn -c:a copy';
-	$filename_append='_audio_only';
-	$record_mode=' *Audio only*';	
-}
-elseif($video)
-{
-	//video only
-	$ffmpeg_arg='-an -c:v copy';
-	$filename_append='_video_only';
-	$record_mode=' *Video only*';	
+	if(AUDIO_ONLY)$video=false;
+	//parse input args , leaves default setting if no args 
+	if(isset($argv[2]) && !empty($argv[2]))
+	{
+		stristr($argv[2],'a')!==false?$audio=true:$audio=false;
+		stristr($argv[2],'v')!==false?$video=true:$video=false;
+		if(stristr($argv[2],'c')!==false)
+		{
+			$video=false;
+			$audio=false;
+		}
+	}
+
+	if($audio && $video) 
+	{
+		//default , record both audio and video
+		$ffmpeg_arg='-c copy';
+	}
+	elseif($audio)
+	{
+		//audio only
+		$ffmpeg_arg='-vn -c:a copy';
+		$filename_append='_audio_only';
+		$record_mode=' *Audio only*';	
+	}
+	elseif($video)
+	{
+		//video only
+		$ffmpeg_arg='-an -c:v copy';
+		$filename_append='_video_only';
+		$record_mode=' *Video only*';	
+	}
 }
 exec('title Twitch Live leecher: '.$channel.' [idle]');
 while(1)
 {
 	if(!$first_run)sleep(IDLE_TIME);
 	$first_run=false;
+
 	if($lastest_vod_ts==0)
-		exec('title Twitch Live leecher: '.$channel.$record_mode.' [idle]');
+		exec('title Twitch Live leecher: '.$channel.$record_mode.' [idle] '.$token_status);
 	else
-		exec('title Twitch Live leecher: '.$channel.$record_mode.' [idle] , lastest VOD recorded at '.date('Ymd H:i:s',$lastest_vod_ts));
+		exec('title Twitch Live leecher: '.$channel.$record_mode.' [idle] , lastest VOD recorded at '.date('Ymd H:i:s',$lastest_vod_ts).'. '.$token_status);
+	
+	$current_ts=$session_ts=time()+$timezone_offset;
 	echo date('Ymd H:i:s',$current_ts).' [INFO] Listening '.$channel.PHP_EOL;
 	
 	//checking channel status
-	$current_ts=$session_ts=time()+28800;
-	$token_request=@file_get_contents('https://api.twitch.tv/api/channels/'.$channel.'/access_token?oauth_token='.OAUTH_TOKEN.'&need_https=true&platform=web&player_type=site&player_backend=mediaplayer&client_id=kimne78kx3ncx6brgo4mv6wki5h1ko');
+	$token_request=@file_get_contents('https://api.twitch.tv/api/channels/'.$channel.'/access_token?oauth_token='.OAUTH_TOKEN.'&need_https=true&platform=_&player_type=site&player_backend=mediaplayer&client_id=kimne78kx3ncx6brgo4mv6wki5h1ko');
+	
+	if(OAUTH_TOKEN!='undefined')
+	{
+		if(strpos($token_request,'"user_id\":null,')!==false)
+			$token_status=' (*token invalid) ';
+		else
+			$token_status=' (token valid) ';
+	}
+	else
+		$token_status=' (not log in) ';
+
 	if($token_request===false)
 	{
-		$msg=date('Ymd H:i:s',$current_ts).' [EROR] Not able to get API of '.$channel.' , maybe incorrect channel spell or got banned?'.PHP_EOL;
+		$msg=date('Ymd H:i:s',$current_ts).' [EROR] Not able to get API of '.$channel.' , maybe incorrect channel ID or banned?'.PHP_EOL;
 		echo $msg;
 		continue;
 	}
@@ -80,6 +110,7 @@ while(1)
 	$https_pos_begin=strpos($usher,'https');
 	$https_pos_end=strpos($usher,'.m3u8',$https_pos_begin);
 	$m3u8_url=substr($usher,$https_pos_begin,$https_pos_end-$https_pos_begin);
+	
 	if(empty($m3u8_url))
 	{
 		$msg=date('Ymd H:i:s',$current_ts).' [EROR] Not able to get correct M3U8 URL of '.$channel.PHP_EOL;
@@ -88,20 +119,40 @@ while(1)
 		continue;
 	}
 	$m3u8_url.='.m3u8';
-	if(!is_dir(VOD_FOLDER))mkdir(VOD_FOLDER);
+		
+	if(strpos(file_get_contents($m3u8_url),',Amazon|')!==false)
+		$ad_exist=true;
+	else
+		$ad_exist=false;
+
+	if($ad_exist)
+	{
+		$current_ts=time()+$timezone_offset;
+		exec('title Twitch Live leecher: '.$channel.$record_mode.' [Playing AD] '.$token_status);
+		$msg=date('Ymd H:i:s',$current_ts).' [INFO] By passing AD';
+		echo $msg;
+		
+		while(true)
+		{
+			echo '.';
+			sleep(4);
+			if(strpos(file_get_contents($m3u8_url),',Amazon|')===false)break;			
+		}
+		echo PHP_EOL;
+	}
 	
 	//downloading VOD via ffmpeg
-	exec('title Twitch Live leecher: '.$channel.$record_mode.' [Recording] , Press "Q" to stop recording');
-	$current_ts=time()+28800;
+	exec('title Twitch Live leecher: '.$channel.$record_mode.' [Recording] , Press "Q" to stop recording. '.$token_status);
+	$current_ts=time()+$timezone_offset;
 	$msg=date('Ymd H:i:s',$current_ts).' [INFO] Record session '.$session_ts.' of '.$channel.$record_mode.' begins'.PHP_EOL;
 	echo $msg;
 	file_put_contents(LOG_FILE,$msg,FILE_APPEND);
-	exec ('ffmpeg -i '.$m3u8_url.' '.$ffmpeg_arg.' '.VOD_FOLDER.DIRECTORY_SEPARATOR.$channel.'-'.date('Ymd_His',$current_ts).$filename_append.'.mp4');
+	exec ('ffmpeg -i '.$m3u8_url.' '.FFMPEG_OPTIONS.' '.$ffmpeg_arg.' '.VOD_FOLDER.DIRECTORY_SEPARATOR.$channel.'-'.date('Ymd_His',$current_ts).$filename_append.'.'.VIDEO_CONTAINER);
 	echo '====================================='.PHP_EOL;
-	$lastest_vod_ts=$current_ts=time()+28800;
-	$msg=date('Ymd H:i:s',$current_ts).' [INFO] Record session '.$session_ts.' of '.$channel.$record_mode.' ends'.PHP_EOL;
+	$lastest_vod_ts=$current_ts=time()+$timezone_offset;
+	$msg=date('Ymd H:i:s',$current_ts).' [INFO] Record session '.$session_ts.' of '.$channel.$record_mode.' ends with '.date('H:i:s',$current_ts-$session_ts).PHP_EOL;
 	echo $msg;
 	file_put_contents(LOG_FILE,$msg,FILE_APPEND);
-	exec('title Twitch Live leecher: '.$channel.$record_mode.' [idle] , lastest VOD recorded at '.date('Ymd H:i:s',$lastest_vod_ts));
+	exec('title Twitch Live leecher: '.$channel.$record_mode.' [idle] , lastest VOD recorded at '.date('Ymd H:i:s',$lastest_vod_ts).'. '.$token_status);
 }
 ?>
